d8b7404e1e9b5c566164844a2ac516f279769d82
Heyang Liu <lhypds@gmail.com>
2026-03-23 16:32:37 +0900

    fix: textUtils

--- src/utils/textUtils.js
+++ src/utils/textUtils.js
@@ -1,23 +1,52 @@
-// .markdown/Note.md -> Category-Note
-// 01_Category 01_Note.md -> Category-Note
+// .markdown/Note.md -> Category[Note]
+// 01_Category 01_Note.md -> Category[Note]
 export function toNoteId(category_, note_) {
-  const note = note_.replace(/^\d+_/, '')
-    .replace('.md', '')
-    .replace('.markdown/', '');
+  let category = category_;
 
-  const category = category_.replace(/^\d+_/, '');
+  // Remove leading digits and underscore
+  category = category.replace(/^\d+_/, '');
+
+  let note = note_;
+
+  // Remove leading digits and underscore
+  note = note.replace(/^\d+_/, '')
+
+  // Remove the extension
+  note = note.replace('.md', '')
+    .replace('.MD', '');
+
+  // Remove the `.markdown/` prefix
+  note = note.replace('.markdown/', '');
+
+  // Replace the whitespace with underscore
+  note = note.replace(/\s+/g, '_');
+
   return `${category}[${note}]`;
 }
 
 // The file name
 // 01_Note.md -> Note
 // .markdown/Note.md -> Note
-export function toNoteTitle(note) {
-  return note.replace('.md', '')
-    .replace('.markdown/', '')
-    .replace(/^\d+_/, '');
+export function toNoteTitle(note_) {
+  let note = note_;
+
+  // Remove leading digits and underscore
+  note = note.replace(/^\d+_/, '')
+
+  // Remove the extension
+  note = note.replace('.MD', '').replace('.md', '');
+
+  // Remove the `.markdown/` prefix
+  note = note.replace('.markdown/', '');
+
+  return note;
 }
 
-export function toCategoryTitle(category) {
-  return category.replace(/^\d+_/, '');
+export function toCategoryTitle(category_) {
+  let category = category_;
+
+  // Remove leading digits and underscore
+  category = category.replace(/^\d+_/, '');
+
+  return category;
 }
