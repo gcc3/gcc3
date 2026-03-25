982621e670856275d103e34308823665c3e7dd94
Heyang Liu <lhypds@gmail.com>
2026-03-25 13:07:31 +0900

    Small fix

--- push-notes.sh
+++ push-notes.sh
@@ -8,6 +8,6 @@ while IFS= read -r -d '' git_dir; do
   dir="$(dirname "$git_dir")"
   echo "Pushing $dir"
   git -C "$dir" add .
-  git -C "$dir" commit -m "Update notes" || echo "Nothing to commit in $dir"
+  git -C "$dir" commit -m "Update notes"
   git -C "$dir" push
 done < <(find "$repo_dir/public/notes" -name ".git" -type d -print0)
